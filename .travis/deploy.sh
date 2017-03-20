357f7d75436e9a075615e5694cdc93690cec60d3
Guillaume Petit <guillaume.petit30@gmail.com>
2017-03-20 17:54:22 +0100

    Configure build

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 BIZDOCK_VERSION=$(cat ../target/version.properties)
+
+# Push version on Docker HUB
 docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
 docker push bizdock/bizdock:${BIZDOCK_VERSION}
+
+# Deploy on CI
