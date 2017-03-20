ccae2daf5cf26a488a037ad62a4e374e18164c62
Guillaume Petit <guillaume.petit30@gmail.com>
2017-03-20 17:18:37 +0100

    Configure build

--- build_image.sh
+++ build_image.sh
@@ -5,7 +5,7 @@
 echo "----------------------------------------"
 echo " Collecting the software components     "
 echo "----------------------------------------"
-mvn -U clean install > /dev/null
+mvn -U clean install
 STATUS=$?
 if [ $STATUS -ne 0 ]; then
   exit 1
