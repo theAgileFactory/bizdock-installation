4b8a7a11098ffeb01860e2372a134b005c9c5b1c
Guillaume Petit <guillaume.petit30@gmail.com>
2017-03-21 11:18:04 +0100

    Configure deploy

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -6,3 +6,4 @@ docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
 docker push bizdock/bizdock:${BIZDOCK_VERSION}
 
 # Deploy on CI
+chmod 600 deploy_key && ssh -i deploy_key ${CI_USER}@ci.bizdock.io ${BIZDOCK_VERSION}
