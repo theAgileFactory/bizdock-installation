00693f927e7003630953012a5f2eee771339c17f
Guillaume Petit <guillaume.petit30@gmail.com>
2017-03-27 14:35:24 +0200

    Configure Travis build

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 BIZDOCK_VERSION=$(cat target/version.properties)
 
-# Push version on Docker HUB
+echo "Pushing BizDock ${BIZDOCK_VERSION} on Docker HUB"
 docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
 docker push bizdock/bizdock:${BIZDOCK_VERSION}
+
+if [ ! -z "$TRAVIS_TAG" ]
+then
+    echo "Tagging and pushing new release with 'latest'"
+    docker tag bizdock/bizdock:${BIZDOCK_VERSION} bizdock/bizdock:latest
+    docker push bizdock/bizdock:latest
+fi
 
 # Deploy on CI
 chmod 600 deploy_key && ssh -i deploy_key ${CI_USER}@ci.bizdock.io ${BIZDOCK_VERSION}
