8cbd37a8ea9cfc466aab21574a8337b572e7a0cf
Guillaume Petit <guillaume.petit30@gmail.com>
2017-09-26 10:04:24 +0200

    Also copy create.sh script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -14,6 +14,9 @@ fi
 
 echo "Deploying on CI"
 chmod 600 deploy_key
+echo "Copying create script"
+wget https://raw.githubusercontent.com/theAgileFactory/bizdock-installation/${TRAVIS_BRANCH}/cli/create.sh
+scp -i deploy_key create.sh ${CI_USER}@ci.bizdock.io:~/scripts/
 echo "Copying test data script"
 wget https://raw.githubusercontent.com/theAgileFactory/maf-desktop-app/${TRAVIS_BRANCH}/development/tools/sample-data/init_data.sql
 scp -i deploy_key init_data.sql ${CI_USER}@ci.bizdock.io:~/scripts/
